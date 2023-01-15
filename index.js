cd43d72361427c1ed349d917a8d955552fc775c1
TOLU-MICH <michealtolu2003@gmail.com>
2023-01-15 13:53:03 -0500

    created an intro page and documented my index.js

--- index.js
+++ index.js
@@ -5,45 +5,65 @@ let score = document.querySelector("#score");
 
 let result = 0;
 let currentTime = timeLeft.textContent;
-let hitPosition;
+let hitPosition, molePosition, timerId;
 
+
+// For each hole container
 for (let i = 1; i < 9; i++) {
   grid.innerHTML += `<div class="square " id=${i}><img src="./dirt.png" alt="hole" class="dirt"></div>`;
 }
+
+document.querySelector(".cancel").onclick = () => {
+  document.querySelector(".menu").style.display = "none";
+  molePosition = setInterval(randomSquare, 1000);
+  timerId = setInterval(countDown, 1000);
+  
+};
+
 const square = document.querySelectorAll(".square");
-
-let molePosition = setInterval(randomSquare, 1000);
+// randomSquare()
 function randomSquare() {
-    setTimeout(() => {
-      document.querySelector(".mole").remove();
-    }, 800);
+  // Tt removes the mole every 800 milliseconds
+  setTimeout(() => {
+    document.querySelector(".mole").remove();
+  }, 800);
 
-  let randomPosition = square[Math.floor(Math.random() * 9)];
+  // set a random position for the  mole
+  let randomPosition = square[Math.floor(Math.random() * 8)];
+  // Set the mole to the random position
   randomPosition.innerHTML += `<img src="./mole.png" alt="mole" class="mole">`;
 
   // assign the id of the randomPosition to hitPosition for us to use later
   hitPosition = randomPosition.id;
 }
 
+// Iterate all dirt container
 square.forEach((id) => {
   id.addEventListener("click", () => {
+    // If the clicked id is the same as the mole id
+    // the player recieves an addition of one point
     if (id.id === hitPosition) {
       result++;
       score.textContent = result;
     }
-    id.innerHTML +=`<img src="./Hammer Clipart 25510.png" alt="hammer" class="hammer">`;
-    setTimeout(()=>{document.querySelector(".hammer").remove()}, 200)
+    id.innerHTML += `<img src="./Hammer Clipart 25510.png" alt="hammer" class="hammer">`;
+    // after 200 milliSeconds it removes the hammer
+    setTimeout(() => {
+      document.querySelector(".hammer").remove();
+    }, 200);
   });
 });
 
-
-let timerId = setInterval(countDown, 1000);
 function countDown() {
   currentTime--;
   timeLeft.textContent = currentTime;
-
+  // If the time is 0 the game ends
   if (currentTime === 0) {
     clearInterval(timerId);
-    alert("GAME OVER! your final score is" + result);
+    clearInterval(molePosition);
+    // alert("GAME OVER! your final score is" + result);
+    document.querySelector("body").innerHTML += `<p class = "notify">GAME OVER!</p>`;
+
+
   }
 }
